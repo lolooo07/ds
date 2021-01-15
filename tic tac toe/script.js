fc163f644735195ea90d12e59ac02cc3e19bff67
lolooo07 <lolooo07@users.noreply.github.com>
2021-01-15 00:00:00 +0000

    remove redundant code for optimization

--- tic tac toe/script.js
+++ tic tac toe/script.js
@@ -69,4 +69,6 @@ reset.addEventListener('click', ()=>{
 console.log("Welcome to Tic Tac Toe")
 let music = new Audio("music.mp3")
 let audioTurn = new Audio("ting.mp3")
-let gameover = new Audio("gameover.mp3")
+let gameover = new Audio("gameover.mp3")
+let turn = "X"
+let isgameover = false;
