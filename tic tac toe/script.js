4f2b925e87f48b35816d2d3dc49a06fa9d1d4bde
lolooo07 <lolooo07@users.noreply.github.com>
2021-01-15 00:00:00 +0000

    fix typo in variable names

--- tic tac toe/script.js
+++ tic tac toe/script.js
@@ -67,4 +67,6 @@ reset.addEventListener('click', ()=>{
 
 
 console.log("Welcome to Tic Tac Toe")
-let music = new Audio("music.mp3")
+let music = new Audio("music.mp3")
+let audioTurn = new Audio("ting.mp3")
+let gameover = new Audio("gameover.mp3")
