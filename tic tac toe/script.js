5a2b85fb06e150089d241a680eb00f02690a7231
lolooo07 <lolooo07@users.noreply.github.com>
2021-01-17 00:00:00 +0000

    fix warnings in codebase

--- tic tac toe/script.js
+++ tic tac toe/script.js
@@ -71,4 +71,6 @@ let music = new Audio("music.mp3")
 let audioTurn = new Audio("ting.mp3")
 let gameover = new Audio("gameover.mp3")
 let turn = "X"
-let isgameover = false;
+let isgameover = false;
+
+// Function to change the turn
