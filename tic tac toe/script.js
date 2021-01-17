be9abf3e2e1480e1137018afc0341bbb9fadd924
lolooo07 <lolooo07@users.noreply.github.com>
2021-01-17 00:00:00 +0000

    cleanup: remove console logs and unused vars

--- tic tac toe/script.js
+++ tic tac toe/script.js
@@ -73,4 +73,6 @@ let gameover = new Audio("gameover.mp3")
 let turn = "X"
 let isgameover = false;
 
-// Function to change the turn
+// Function to change the turn
+const changeTurn = ()=>{
+    return turn === "X"? "0": "X"
