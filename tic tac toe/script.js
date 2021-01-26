97180a42a00e29209eb17a3f320b7c88abb03792
lolooo07 <lolooo07@users.noreply.github.com>
2021-01-26 00:00:00 +0000

    refactor: minor code structure improvements

--- tic tac toe/script.js
+++ tic tac toe/script.js
@@ -97,4 +97,6 @@ const checkWin = ()=>{
             isgameover = true
             document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
             document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
-            document.querySelector(".line").style.width = "20vw";
+            document.querySelector(".line").style.width = "20vw";
+        }
+    })
