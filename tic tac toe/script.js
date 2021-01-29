2f80db9806787ecb0b57ed0ad83d8720be6fc235
lolooo07 <lolooo07@users.noreply.github.com>
2021-01-29 00:00:00 +0000

    small tweaks and code enhancements

--- tic tac toe/script.js
+++ tic tac toe/script.js
@@ -105,4 +105,6 @@ const checkWin = ()=>{
 // Game Logic
 // music.play()
 let boxes = document.getElementsByClassName("box");
-Array.from(boxes).forEach(element =>{
+Array.from(boxes).forEach(element =>{
+    let boxtext = element.querySelector('.boxtext');
+    element.addEventListener('click', ()=>{
