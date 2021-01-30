0a778810d26aa4e639048938bca718db5a09d77e
lolooo07 <lolooo07@users.noreply.github.com>
2021-01-30 00:00:00 +0000

    chore: clean up unused code

--- tic tac toe/script.js
+++ tic tac toe/script.js
@@ -109,4 +109,6 @@ Array.from(boxes).forEach(element =>{
     let boxtext = element.querySelector('.boxtext');
     element.addEventListener('click', ()=>{
         if(boxtext.innerText === ''){
-            boxtext.innerText = turn;
+            boxtext.innerText = turn;
+            turn = changeTurn();
+            audioTurn.play();
